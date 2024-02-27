d769b74526ad3bcbcde439d52378ae79007d6ae0
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-02-27 21:10:14 +0530

    new Chang

--- mini/src/App.js
+++ mini/src/App.js
@@ -1,43 +1,45 @@
+import "./App.css";
+import React, { useState } from "react";
 
-import './App.css';
-import React, { useState } from 'react'
+const dictionary = [
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
 
-const dictionary = 
-[
+  { word: "Component", meaning: "A reusable building block in React." },
 
-    { word: "React", meaning: "A JavaScript library for building user interfaces." },
-
-    { word: "Component", meaning: "A reusable building block in React." },
-
-    { word: "State", meaning: "An object that stores data for a component." }
-
-]
-
-
-
+  { word: "State", meaning: "An object that stores data for a component." },
+];
 
 function App() {
-  const [input,setInput] = useState("");
-  const [definition,setDefinition] =useState("")
+  const [input, setInput] = useState("");
+  const [definition, setDefinition] = useState("");
 
- const handleSearch = ()=>{
-  const words = input.toLowerCase()
-  const findWord = dictionary.find((entry)=> entry.word.toLowerCase() === words)
-  if(findWord){
-    setDefinition(findWord.meaning)
-    setInput("")
-  }else{
-    setDefinition("Word not found in the dictionary.");
-  }
- }
+  const handleSearch = () => {
+    const words = input.toLowerCase();
+    const findWord = dictionary.find(
+      (entry) => entry.word.toLowerCase() === words
+    );
+    if (findWord) {
+      setDefinition(findWord.meaning);
+      setInput("");
+    } else {
+      setDefinition("Word not found in the dictionary.");
+    }
+  };
   return (
     <div className="App">
       <h1>Dictionary App</h1>
-      <input value={input} type="text" placeholder="Search for a word..." onChange={(e)=> setInput(e.target.value)} />
+      <input
+        value={input}
+        type="text"
+        placeholder="Search for a word..."
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button onClick={handleSearch}>Search</button>
-      <p>Definition:
-        {definition}
-        </p>      
+      <p>Definition:</p>
+      <p>{definition}</p>
     </div>
   );
 }
